3fcc9416c8bda9ec72d5f92e168288cb2e2e0a7e
dirokim <dirokim7@gmail.com>
2024-04-05 16:01:40 +0900

    feat setting

---
+++ src/main/java/com/disaster/app/DisasterApiApplication.java
@@ -0,0 +1,13 @@
+package com.disaster.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisasterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisasterApiApplication.class, args);
+	}
+
+}
