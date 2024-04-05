7e9429bac1c0563754e178214a7b19369e03e2b9
dirokim <dirokim7@gmail.com>
2024-04-05 17:32:36 +0900

    feat setting

--- src/main/java/com/disaster/app/TestController.java
+++ src/main/java/com/disaster/app/TestController.java
@@ -4,11 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
 @Controller
 public class TestController {
 
 	@RequestMapping( value = "/" , method = RequestMethod.GET) 
-	public void test() throws Exception {
-		
+	public String test() throws Exception {
+
+		URL url = new URL("http://apis.data.go.kr/1741000/DisasterMsg3/getDisasterMsg1List");
+		HttpURLConnection connection =  (HttpURLConnection)url.openConnection();
+		connection.setRequestMethod("GET");
+		connection.setRequestProperty("Content-type","application/json");
+
+		connection.se
+
+
+
+
+
+
+
+		return "index";
 	}
 }
